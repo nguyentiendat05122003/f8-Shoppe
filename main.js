fbd0c0dbf6eba0685cd7947d93cac7bcd059da95
nguyentiendat05122003 <95393228+nguyentiendat05122003@users.noreply.github.com>
2022-07-27 00:06:18 +0700

    Final Changes

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const headerCartElement = document.querySelector('.header-cart');
 const headerHasCart = document.querySelector('.header-cart-hasItem');
 const headerHistory = document.querySelector('.wrapper-input');
 const InputElement = document.querySelector('.header-search-input');
-const cartElement = document.querySelector('.header-cart-icon');
 btnSearch.addEventListener('click',(e)=>{
     if(headerSearch.classList.contains('show')){
        headerSearch.classList.remove('show');
@@ -17,15 +16,12 @@ document.body.addEventListener('click',(e)=>{
     if(headerSearch.classList.contains('show') && e.target !== btnSearch && e.target !== headerSearch && e.target!==InputElement){
         headerSearch.classList.remove('show');
     }
-})
-headerCartElement.addEventListener('click',(e)=>{
-    if(headerHasCart.classList.contains('show-cart')){
-        headerHasCart.classList.remove('show-cart');
-    }
-    else{
-        headerHasCart.classList.add('show-cart');
-    }
-})
+ })
+// headerCartElement.addEventListener('click',(e)=>{  
+//         headerHasCart.classList.toggle('is-showCart')
+// })
+
+
 // document.body.addEventListener('click',(e)=>{
 //     if(e.target !== headerCartElement && headerHasCart.classList.contains('show-cart') && e.target !==headerHasCart){
 //         headerHasCart.classList.remove('show-cart');
